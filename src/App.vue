450a0fed8dc38ba9851af6aa7aed5ecc0f8bdece
alip <alifrizkipambudi@gmail.com>
2017-12-31 05:55:02 +0700

    create App component

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ul v-for="todo in $data.todos.filter(shoudShowTodo)">
+      <li v-bind:class="{ complete: todo.done }">
+        {{ todo.text }}
+        <input type="checkbox" v-model="todo.done">
+      </li>
+    </ul>
+    <form v-on:submit="handleAddTodo($event)">
+      <input type="text" v-model="input">
+    </form>
+    <label class="show-completed">
+      Show completed items?
+      <input type="checkbox" v-model="showDone">
+    </label>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      handleAddTodo(e) {
+        e.preventDefault()
+        this.$data.todos.push({ text: this.input, done: false })
+        this.input = ''
+      },
+      shoudShowTodo(todo) {
+        if (this.$data.showDone) {
+          return true
+        } else {
+          return !todo.done
+        }
+      }
+    },
+    data() {
+      return this.$props.state
+    },
+    props:['state']
+  }
+</script>
+
+<style>
+  .complete {
+    color: gainsboro;
+    text-decoration: line-through;
+    font-style: italic;
+  }
+</style>
